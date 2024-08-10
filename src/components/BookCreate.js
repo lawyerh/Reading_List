0188c3a159b01519b959af18cf87e5434e3ce230
lawyerh <lawyerh01@gmail.com>
2024-08-09 21:21:31 -0700

    Can now toggle book create window by clicking the button on bottom of page

--- src/components/BookCreate.js
+++ src/components/BookCreate.js
@@ -4,7 +4,8 @@ import useGoogleContext from "../hooks/useGoogleContext";
 
 function BookCreate(){
     // LOCAL STATE
-    const [query, setQuery] = useState("")
+    const [query, setQuery] = useState("");
+    const [active, setActive] = useState(false);
 
     // APP STATE
     const {bookSearchByTitle} = useGoogleContext();
@@ -13,14 +14,19 @@ function BookCreate(){
         setQuery(e.target.value);
     }
 
+    const handleClick = () => {
+        setActive(!active)
+    }
+
     const handleSubmit = (e) => {
         e.preventDefault();
         bookSearchByTitle(query);
         setQuery('');
     }
     return(
-        <div className="create">
-            <h2 className="create__heading">Enter a book title to</h2>
+        // Input needs validation
+        <div className={`create${active ? " create__active" : ""}`}>
+            <button onClick={handleClick} className="btn create__heading">Search Google Books</button>
             <form onSubmit={handleSubmit} className="create__form">
                 <input value={query} onChange={handleChange} className="create__input" />
                 <button className="btn create__button">
